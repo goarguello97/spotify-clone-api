bfe9aa8ee54fd43e37c66ab387f8a6d71d98638a
Gonzalo Argüello <arguellogonzalo97@gmail.com>
2023-11-24 12:16:42 -0300

    Create controllers

---
+++ src/controllers/scopes.js
@@ -0,0 +1,9 @@
+export default info = [
+  "user-read-email", // email info
+  "user-read-private", // user private info (not sensitive data)
+  "playlist-read-private", // fetch private playlists
+  "playlist-modify-public", // create public playlists
+  "playlist-modify-private", // delete tracks from playlist
+  "user-follow-read", // fetch what user follows
+  "user-library-read",
+];
